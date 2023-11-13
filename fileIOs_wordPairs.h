ce82d76d3b727ad5e63228b0d436133697a80350
Andy Poon <andypoon923@gemtrifi.com>
2023-11-12 23:47:08 -0800

    added declaration fix

--- fileIOs_wordPairs.h
+++ fileIOs_wordPairs.h
@@ -7,7 +7,7 @@
 #include <set>
 
 
-void sentenceSplitter(std::string& fname, std::vector<std::string>& sentences);
+void sentenceSplitter(const std::string& fname, std::vector<std::string>& sentences);
 void wordpairMapping( std::vector<std::string>& sentences, std::map< std::pair<std::string,std::string>, int> &wordpairFreq_map);
 void freqWordpairMmap(std::map< std::pair<std::string,std::string>, int> &wordpairFreq_map, std::multimap<int, std::pair<std::string, std::string> > &freqWordpair_mmap );
 void printWordpairs(std::multimap<int, std::pair<std::string, std::string> > &freqWordpair_multimap, std::string outFname, int topCnt, int botCnt);
